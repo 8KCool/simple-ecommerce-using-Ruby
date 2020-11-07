6f0fd1cd35e8048dd2ce277a2712ab6afc3d9761
BlackPandalancer <blackpandalancer@gmail.com>
2020-11-06 22:36:52 -0300

    creates category model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_232606) do
+ActiveRecord::Schema.define(version: 2020_11_07_010850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false

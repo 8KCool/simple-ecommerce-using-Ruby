97e2e8a934d6a066bac7645f28e39623e07c29aa
BlackPandalancer <blackpandalancer@gmail.com>
2020-11-10 20:01:01 -0300

    creates aux method to requests test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { Faker::Name.email }
+    name { Faker::Name.name }
     email { Faker::Internet.email }
     password { '123456' }
     password_confirmation { '123456' }

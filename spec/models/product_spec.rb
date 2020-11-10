9508ee749c8a9363e171a0eb783e07ddb9a870f7
BlackPandalancer <blackpandalancer@gmail.com>
2020-11-09 22:15:13 -0300

    add image to product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,4 +12,6 @@ RSpec.describe Product, type: :model do
 
   it { is_expected.to have_many(:product_categories).dependent(:destroy) }
   it { is_expected.to have_many(:categories).through(:product_categories) }
+
+  it { is_expected.to validate_presence_of(:image) }
 end

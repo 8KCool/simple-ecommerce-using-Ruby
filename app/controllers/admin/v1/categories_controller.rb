2e8ae2eaf8598147b6965ced36a88b69c810d53c
BlackPandalancer <blackpandalancer@gmail.com>
2020-11-10 21:34:39 -0300

    implements category create method and tests

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -4,6 +4,27 @@ module Admin
       def index
         @categories = Category.all
       end
+
+      def create
+        @category = Category.new
+        @category.attributes = category_params
+        save_category!
+      end
+
+      private
+
+      def category_params
+        return unless params.has_key?(:category)
+
+        params.require(:category).permit(:name)
+      end
+
+      def save_category!
+        @category.save!
+        render :show
+      rescue
+        render_error(fields: @category.errors.messages)
+      end
     end
   end
 end

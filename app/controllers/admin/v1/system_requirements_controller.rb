00de9aef31d237a92b1759e8b58613988cd794d0
BlackPandalancer <blackpandalancer@gmail.com>
2020-11-14 10:40:07 -0300

    add system requirement create method and tests

--- app/controllers/admin/v1/system_requirements_controller.rb
+++ app/controllers/admin/v1/system_requirements_controller.rb
@@ -4,6 +4,27 @@ module Admin
       def index
         @system_requirements = SystemRequirement.all
       end
+
+      def create
+        @system_requirement = SystemRequirement.new
+        @system_requirement.attributes = system_requirement_params
+        save_system_requirement!
+      end
+
+      private
+
+      def system_requirement_params
+        return unless params.key?(:system_requirement)
+
+        params.require(:system_requirement).permit(:name, :operational_system, :storage, :processor, :memory, :video_board)
+      end
+
+      def save_system_requirement!
+        @system_requirement.save!
+        render :show
+      rescue
+        render_error(fields: @system_requirement.errors.messages)
+      end
     end
   end
 end
--- spec/requests/admin/v1/system_requirements/admin_requests_spec.rb
+++ spec/requests/admin/v1/system_requirements/admin_requests_spec.rb
@@ -17,4 +17,51 @@ RSpec.describe 'Admin::V1::SystemRequirements as :admin', type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  context 'POST /system_requiments' do
+    let(:url) { '/admin/v1/system_requirements' }
+
+    context 'with valid params' do
+      let(:system_requirement_params) { { system_requirement: attributes_for(:system_requirement) }.to_json }
+
+      it 'adds a new system_requirement' do
+        expect do
+          post url, headers: auth_header(user), params: system_requirement_params
+        end.to change(SystemRequirement, :count).by(1)
+      end
+
+      it 'returns last added system_requirement' do
+        post url, headers: auth_header(user), params: system_requirement_params
+        expect_system_requirement = SystemRequirement.last.as_json(only: %i(id name operational_system storage processor memory video_board))
+        expect(body_json['system_requirement']).to eq(expect_system_requirement)
+      end
+
+      it 'returns success status' do
+        post url, headers: auth_header(user), params: system_requirement_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'with invalid params' do
+      let(:system_requirement_invalid_params) do
+        { system_requirement: attributes_for(:category, storage: nil) }.to_json
+      end
+
+      it 'does not add a new SystemRequirement' do
+        expect do
+          post url, headers: auth_header(user), params: system_requirement_invalid_params
+        end.to_not change(SystemRequirement, :count)
+      end
+
+      it 'returns error messages' do
+        post url, headers: auth_header(user), params: system_requirement_invalid_params
+        expect(body_json['errors']['fields']).to have_key('storage')
+      end
+
+      it 'returns unprocessable_entity status' do
+        post url, headers: auth_header(user), params: system_requirement_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end

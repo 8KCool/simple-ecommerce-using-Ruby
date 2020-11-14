7dedc87e24b7dc34042b5975e145fdc34f48d8a0
BlackPandalancer <blackpandalancer@gmail.com>
2020-11-13 21:08:49 -0300

    validates admin access

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -1,14 +1,28 @@
 module Admin
   module V1
     class ApiController < ApplicationController
+      class ForbiddenAccess < StandardError; end
+
       include Authenticable
+
+      before_action :restrict_access_for_admin!
 
       def render_error(message: nil, fields: nil, status: :unprocessable_entity)
         errors = {}
         errors['fields'] = fields if fields.present?
-        errors['message'] = fields if message.present?
+        errors['message'] = message if message.present?
 
         render json: { errors: errors }, status: status
+      end
+
+      rescue_from ForbiddenAccess do
+        render_error(message: 'Forbidden access', status: :forbidden)
+      end
+
+      private
+
+      def restrict_access_for_admin!
+        raise ForbiddenAccess unless current_user.admin?
       end
     end
   end

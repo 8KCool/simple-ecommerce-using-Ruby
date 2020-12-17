732979396273b48ffb392f5f6e588ec0d1906b64
BlackPandalancer <blackpandalancer@gmail.com>
2020-12-16 21:27:41 -0300

    fixes devise permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:signup, keys: %i[name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
   end
 end
